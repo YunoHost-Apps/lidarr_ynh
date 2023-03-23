beca96507771efbf3ebea9e6caa91f23204ef7ea
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2023-03-23 23:13:14 +0100

    v2

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,7 +5,7 @@
 #=================================================
 
 # dependencies used by the app
-pkg_dependencies="curl mediainfo sqlite3"
+#REMOVEME? pkg_dependencies="curl mediainfo sqlite3"
 
 #=================================================
 # PERSONAL HELPERS
